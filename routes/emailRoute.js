9c605a89bed42e15f31b17376916de3c58bd2205
Alina Nadtochiy <am0817@mail.ru>
2025-05-28 23:15:24 +0300

    add changes in email Route

--- routes/emailRoute.js
+++ routes/emailRoute.js
@@ -1,7 +1,13 @@
 import express from "express";
+import multer from "multer";
 import { sendEmail } from "../controllers/sendEmail.js";
 
 const router = express.Router();
-router.post("/", sendEmail);
+// Настраиваем multer
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+// Оборачиваем маршрут в multer
+router.post("/", upload.single("file"), sendEmail);
 
 export default router;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bodyParser from "body-parser";
+// import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 
@@ -10,8 +10,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 
@@ -27,6 +27,8 @@ mongoose
   )
   .then(() => console.log("DB OK"))
   .catch(err => console.log("BD error", err));
+
+console.log("Настройка маршрутов завершена, запускаем сервер...");
 
 app.listen(port, () => {
   console.log(`Is listening on port: ${port}`);
